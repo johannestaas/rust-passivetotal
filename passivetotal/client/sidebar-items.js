4e41b93107bc3d96da882aad5f2f77a0863dc149
Johan Nestaas <johannestaas@gmail.com>
2016-03-30 23:43:15 -0700

    update

--- passivetotal/client/sidebar-items.js
+++ passivetotal/client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["PTClient","This is the client exposed to the user for abstracting the passivetotal API."]]});
+initSidebarItems({"enum":[["ResponseError",""]],"struct":[["PTClient","This is the client exposed to the user for abstracting the passivetotal API."]]});
