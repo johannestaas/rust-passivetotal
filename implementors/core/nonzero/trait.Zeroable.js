805e3effc94b790f40dddcddc0ccf14614f9005e
Johan Nestaas <johannestaas@gmail.com>
2016-03-30 23:04:59 -0700

    add crates

---
+++ implementors/core/nonzero/trait.Zeroable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bitflags'] = [];implementors['libc'] = [];implementors['hyper'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];implementors['passivetotal'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
